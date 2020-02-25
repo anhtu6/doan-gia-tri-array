5149df836e2dbb0c67bf6250526f1be2eaa104b2
anhtu6 <anhtu632@gmail.com>
2020-02-25 11:17:52 +0700

    doan gia tri trong array

---
+++ js/findvalue.js
@@ -0,0 +1,7 @@
+let arraySample = [1,2,6,15,21,33];
+let n = parseInt(prompt('Nhap so du doan cua ban'));
+for (i=0;i<arraySample.length;i++){
+    if (arraySample[i] ==n) {
+        alert('Gia tri ton tai trong mang o vi tri '+i);
+    }
+}
